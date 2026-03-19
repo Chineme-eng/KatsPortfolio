18a4236fd6c0a75582bf9124f678447540ab839c
KatOduah <chinemeoduah@gmail.com>
2026-03-19 12:46:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,3 @@ document.querySelectorAll(".nav-links a").forEach((link) => {
     navLinks.classList.remove("show");
   });
 });
-
-const form = document.querySelector(".contact-form");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  alert("Your form is currently a front-end placeholder. Later, you can connect it to Formspree or another form service.");
-  form.reset();
-});
